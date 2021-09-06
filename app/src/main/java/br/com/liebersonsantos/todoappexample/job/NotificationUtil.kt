716a7d3d6cff88b89058f867277431b65130297f
liebersonsantos <lieberson@lieberson-2.local>
2021-09-06 11:52:48 -0300

    create pendingIntetNav

--- app/src/main/java/br/com/liebersonsantos/todoappexample/job/NotificationUtil.kt
+++ app/src/main/java/br/com/liebersonsantos/todoappexample/job/NotificationUtil.kt
@@ -2,12 +2,16 @@ package br.com.liebersonsantos.todoappexample.job
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
 import android.content.Context.NOTIFICATION_SERVICE
 import android.graphics.Color
 import android.os.Build
+import android.os.Bundle
 import androidx.core.app.NotificationCompat
+import androidx.navigation.NavDeepLinkBuilder
 import br.com.liebersonsantos.todoappexample.R
+import br.com.liebersonsantos.todoappexample.ui.fragment.addtaskfragment.TASK_ID
 
 /**
  * Created by lieberson on 9/1/21.
@@ -22,20 +26,39 @@ object NotificationUtil {
         val notificationManager = context.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
         createChannel(notificationManager, context)
         val notificationBuilder: NotificationCompat.Builder =
-            getNotificationBuilder(context, taskName)
+            getNotificationBuilder(context, taskName, taskId)
 
         notificationManager.notify(taskId.toInt(), notificationBuilder.build())
 
     }
 
-    private fun getNotificationBuilder(context: Context, taskName: String): NotificationCompat.Builder {
+    private fun getNotificationBuilder(context: Context, taskName: String, taskId: Long): NotificationCompat.Builder {
        return NotificationCompat.Builder(context, PRIMARY_CHANNEL_ID)
            .setSmallIcon(R.drawable.ic_notification)
            .setContentTitle(taskName)
            .setContentText(context.getString(R.string.notification_text))
+           .setAutoCancel(true)
            .setPriority(NotificationCompat.PRIORITY_HIGH)
+           .setContentIntent(pendingIntentNav(context, taskId))
     }
 
+//    private fun pendingIntent(context: Context, taskId: Long): PendingIntent {
+//        val intent = Intent(context, MainActivity::class.java)
+//        intent.putExtra("toFragment", taskId.toInt())
+//        return PendingIntent.getActivity(context, taskId.toInt(), intent, PendingIntent.FLAG_UPDATE_CURRENT)
+//    }
+
+    private fun pendingIntentNav(context: Context, taskId: Long): PendingIntent {
+        val bundle = Bundle().apply {
+            putLong(TASK_ID, taskId)
+        }
+
+        return NavDeepLinkBuilder(context)
+            .setGraph(R.navigation.nav_graph)
+            .setDestination(R.id.detailFragment)
+            .setArguments(bundle)
+            .createPendingIntent()
+    }
 
     private fun createChannel(notificationManager: NotificationManager, context: Context) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
--- app/src/main/java/br/com/liebersonsantos/todoappexample/ui/fragment/detailfragment/DetailFragment.kt
+++ app/src/main/java/br/com/liebersonsantos/todoappexample/ui/fragment/detailfragment/DetailFragment.kt
@@ -5,7 +5,6 @@ import android.view.*
 import android.widget.AdapterView
 import android.widget.AdapterView.OnItemSelectedListener
 import android.widget.ArrayAdapter
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
